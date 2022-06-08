79400fa603e82cd3b6128ac7ddce660816903464
mihail1409 <aleksandrov20012@mail.ru>
2022-06-08 13:45:37 +0300

    Задание по рендеру React

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+    function callback(message: number) {
+        setMessage(message);
+    }
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+            setMessage(-1);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
